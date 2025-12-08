72241cd6181d7d7af3b28fec3a6172dc9e6189c9
mezz01 <mezzatamine2002@gmail.com>
2025-12-08 23:37:59 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+  plugins: [tailwindcss()],
+},
+site: "https://kwesk.ma",
+
+  integrations: [sitemap(
+    {
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }
+  )],
+});
